3a7c76b9d7e688526f74ecd672b0887ac654f0a2
CHIPK <chipk215@msn.com>
2018-09-26 11:01:12 -0600

    Added categorical features Embarked and Cabin

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -23,6 +23,16 @@ def get_missing_feature_list(df):
     return missing_feature_list
 
 
+def handle_embark_missing_values(df):
+    """
+    Majority of passengers embarked in Southampton
+    :param df:
+    :return:
+    """
+    df.loc[df.Embarked.isnull(), 'Embarked'] = 'S'
+    return df
+
+
 def handle_age_missing_values(df):
     """
     Replace missing age values for passengers using the average age of persons in the same passenger class.
@@ -60,12 +70,26 @@ def handle_fare_missing_values(df):
     return df
 
 
+def handle_cabin_missing_values(df):
+
+    def truncate_cabin_name(x):
+        try:
+            return x[0]
+        except TypeError:
+            return 'None'
+
+    df['Cabin'] = df.Cabin.apply(truncate_cabin_name)
+    return df
+
+
 def handle_missing_features(missing_features, df):
     if len(missing_features) == 0:
         return df
 
     feature_handlers = {'Age': handle_age_missing_values,
-                        'Fare': handle_fare_missing_values}
+                        'Fare': handle_fare_missing_values,
+                        'Embarked': handle_embark_missing_values,
+                        'Cabin': handle_cabin_missing_values}
 
     for feature in missing_features:
         print("Feature missing data:", feature)
@@ -83,48 +107,62 @@ def process_training_data(df_train):
     missing_features = get_missing_feature_list(df_train)
     df_train = handle_missing_features(missing_features, df_train)
     # replace Sex attribute with numeric encoding
-    df_train = pd.get_dummies(df_train, columns=['Sex'], drop_first=True)
+    df_train = pd.get_dummies(df_train, columns=['Sex', 'Embarked', 'Cabin'], drop_first=True)
     y_predict = df_train['Survived']
 
-    # Drop PassengerId, Survived, Name, Cabin, Embarked, and Ticket features for use in model
-    predict_features = df_train.drop(['PassengerId', 'Survived', 'Name', 'Cabin', 'Embarked', 'Ticket'], axis=1)
+    # Drop PassengerId, Survived, Name and Ticket features for use in model
+    predict_features = df_train.drop(['PassengerId', 'Survived', 'Name', 'Ticket'], axis=1)
 
     train_X, val_X, train_y, val_y = train_test_split(predict_features, y_predict,  random_state=214)
-    tree_count = 700
-    rf_classifier = RandomForestClassifier(tree_count, random_state=214)
+    tree_count = 1000
+    rf_classifier = RandomForestClassifier(tree_count, n_jobs=-1, max_features='sqrt',
+                                           min_samples_leaf=1, bootstrap=True, random_state=42)
     rf_classifier.fit(train_X, train_y)
+    score = rf_classifier.score(train_X, train_y)
+    print("Model score= ", score)
     survivor_predictions = rf_classifier.predict(val_X)
     print(confusion_matrix(val_y, survivor_predictions))
     tn, fp, fn, tp = confusion_matrix(val_y, survivor_predictions).ravel()
     training_accuracy = compute_accuracy(tn, tp, val_X.shape[0])
     print("Training accuracy =", training_accuracy)
 
-    return rf_classifier
+    return rf_classifier, list(predict_features)
 
 
-def run_main():
-    df_train, df_test = read_data_files()
-    rf_classifier = process_training_data(df_train)
-
+def process_test_data(df_test, rf_classifier, training_features):
     # Predict survivors using test data
     missing_features = get_missing_feature_list(df_test)
 
-    # Drop PassengerId, Name, Cabin, Embarked, and Ticket features for use in model
+    # Drop PassengerId, Name and Ticket features for use in model
 
     df_test = handle_missing_features(missing_features, df_test)
     # replace Sex attribute with numeric encoding
-    df_test = pd.get_dummies(df_test, columns=['Sex'], drop_first=True)
-    predict_features = df_test.drop(['PassengerId', 'Name', 'Cabin', 'Embarked', 'Ticket'], axis=1)
+    df_test = pd.get_dummies(df_test, columns=['Sex', 'Embarked', 'Cabin'], drop_first=True)
+    predict_features = df_test.drop(['PassengerId', 'Name', 'Ticket'], axis=1)
+
+    # Ensure training features and test features match.
+    # The issue is that test data may not have a specific instance for a categorical feature that
+    # has been represented as a dummy
+    test_features = list(predict_features)
+    missing_features = list(set(training_features).difference(test_features))
+    for feature in missing_features:
+        predict_features[feature] = 0
+
     test_predictions = rf_classifier.predict(predict_features)
 
     passenger_ids = df_test['PassengerId']
     print(type(test_predictions), test_predictions.shape)
-    # result_data = [[ df_test['PassengerId'], test_predictions[0]]]
-
     result = pd.DataFrame(passenger_ids)
     result['Survived'] = test_predictions
 
     result.to_csv('results/test_results.csv', encoding='utf-8', index=False)
+
+
+def run_main():
+    df_train, df_test = read_data_files()
+    rf_classifier, training_features = process_training_data(df_train)
+    # Predict survivors using test data
+    process_test_data(df_test, rf_classifier, training_features)
 
 
 if __name__ == "__main__":
